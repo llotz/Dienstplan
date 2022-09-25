75d22e47711037b90880416762dc56cdba382dcf
Lukas Lotz <11498557+llotz@users.noreply.github.com>
2022-09-25 13:36:30 +0200

    Adding Filter Button

--- src/views/main.php
+++ src/views/main.php
@@ -1,16 +1,26 @@
 <?
-    // Load Data Dependencies
-    include_once(getViewModel("main"));
+// Load Data Dependencies
+include_once(getViewModel("main"));
 ?>
 
 <h3>Übersicht</h3>
+<div class="main-filter">
     <form method='GET' class="filter">
         <div class="filter-box">
-			<!--<div class="filter-item">Nächste <?=GetHtmlSelector("interval", $intervalList, "Value", "Output", $interval, false)?> Tage</div>-->
-            <div class="filter-item"><?=GetHtmlSelector("department", $departments, "Id", "Name", $departmentId, true, "Organisator")?></div>
-            <div class="filter-item"><?=GetHtmlSelector("sector", $sectors, "Id", "Name", $sectorId, true, "Abteilung")?></div>
-            <div class="filter-item"><input class="flex-box-button" type="submit" value="Filter Anwenden"/></div>
+            <!--<div class="filter-item">Nächste <?= GetHtmlSelector("interval", $intervalList, "Value", "Output", $interval, false) ?> Tage</div>-->
+            <div class="filter-item"><?= GetHtmlSelector("department", $departments, "Id", "Name", $departmentId, true, "Organisator") ?></div>
+            <div class="filter-item"><?= GetHtmlSelector("sector", $sectors, "Id", "Name", $sectorId, true, "Abteilung") ?></div>
+            <div class="filter-item"><input class="flex-box-button" type="submit" value="Filter anwenden" /></div>
+
+            <? if ($departmentId != 0 || $sectorId != 0) { ?>
+                <div class="delete-filter"></div>
+                <a href="/">
+                    <div class="filter-item"><button class="button-red" type="button">Filter löschen</button></div>
+                </a>
+            <? } ?>
         </div>
     </form>
-        
-<?=$grid?>
+</div>
+
+
+<?= $grid ?>
